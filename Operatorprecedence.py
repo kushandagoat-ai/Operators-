0088083f890ecdc3291c86b4ca07f90d6c3e2b3f
kushandagoat-ai <kushandagoat@gmail.com>
2026-01-12 16:21:16 -0800

    Add files via upload

---
+++ Operatorprecedence.py
@@ -0,0 +1,13 @@
+w = 1
+v = 2
+y = 3
+z = 4
+z = (v*v*w)+8/4
+print("value of",z)
+name = "Kushan"
+age = 13
+
+if name == "Kushan" or name == "bob" and age >7:
+    print("This is true")
+else :
+    print("This is not true")
