5da103f2f35ed0ab345642899943b44da02db0f5
kushandagoat-ai <kushandagoat@gmail.com>
2026-01-12 16:38:57 -0800

    Add files via upload

---
+++ dvisiblenumber.py
@@ -0,0 +1,7 @@
+
+numn = int(input("Please enter number here"))
+print(numn)
+numd = int(input("Please enter another number here"))
+print(numd)
+Div = numn/numd
+print("The value of the numerator divided by the denominator is",Div)
